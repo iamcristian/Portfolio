5da59527ebbb321953879a5c44a5b0273b70f9a6
cristian <crisarandosyse@gmail.com>
2024-03-23 16:23:23 -0400

    Add links in Home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { useAppContext } from "@/context/ConfigContext";
 import Image from "next/image";
 import { GrDownload, GrLinkedin, GrMail } from "react-icons/gr";
 import { ReactTyped } from "react-typed";
 
+import esTranslations from "../../translations/es.json";
+import enTranslations from "../../translations/en.json";
+import Link from "next/link";
+
 export const Home = () => {
+  const { language } = useAppContext();
+  const translations = language === "es" ? esTranslations : enTranslations;
+  const { presentation, job, situation, description, contactMe, downloadCV } =
+    translations.home;
   return (
     <section
       className="section-page flex flex-col justify-center pt-32 pb-0 h-[70vh] font-medium md:justify-center"
@@ -18,39 +27,49 @@ export const Home = () => {
           width={50}
           height={50}
         />
-        <p className="text-sm lg:text-lg">Hello! 👋, I am</p>
+        <p className="text-sm lg:text-lg">{presentation}</p>
       </div>
       <h1 className="text-2xl lg:text-3xl font-bold tracking-tight dark:text-white">
         Cristian Arando
       </h1>
       <ReactTyped
         className="text-base font-semibold"
-        strings={["Software Developer"]}
+        strings={[job]}
         typeSpeed={100}
         backDelay={5000}
         loop
       />
       <p className="font-light my-5 w-[60%]">
         <strong className="text-sky-700 font-semibold dark:text-sky-500">
-          Estudiante de Ingenieria de Sistemas y Desarrollador de Software
+          {situation}
         </strong>{" "}
-        de Cochabamba, Bolivia. Especializado en el desarrollo de aplicaciones
-        web.
+        {description}
       </p>
 
       <div className="flex gap-5">
-        <button className="flex items-center gap-2 text-xs rounded-lg border border-black dark:border-white p-2 dark:bg-slate-900 hover:bg-slate-50 dark:hover:bg-slate-800 text-black dark:text-white transition-colors duration-700">
+        <Link
+          href="mailto:crisarandosyse@gmail.com"
+          className="flex items-center gap-2 text-xs rounded-lg border border-black dark:border-white p-2 dark:bg-slate-900 hover:bg-slate-50 dark:hover:bg-slate-800 text-black dark:text-white transition-colors duration-700"
+        >
           <GrMail size={20} />
-          Contact me
-        </button>
-        <button className="flex items-center gap-2 text-xs rounded-lg border p-2 border-black dark:border-white dark:bg-slate-900 hover:bg-slate-50 dark:hover:bg-slate-800 text-black dark:text-white transition-colors duration-700">
+          {contactMe}
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/cristianarando"
+          target="_blank"
+          className="flex items-center gap-2 text-xs rounded-lg border p-2 border-black dark:border-white dark:bg-slate-900 hover:bg-slate-50 dark:hover:bg-slate-800 text-black dark:text-white transition-colors duration-700"
+        >
           <GrLinkedin size={20} />
           Linkedin
-        </button>
-        <button className="flex items-center gap-2 text-xs rounded-lg border p-2 bg-slate-900 hover:bg-slate-700 text-white dark:bg-sky-700 dark:hover:bg-sky-600 border-black dark:border-solid transition-colors duration-700">
+        </Link>
+        <Link
+          href={language === "en" ? "/CV_EN.pdf" : "/CV_ES.pdf"}
+          target="_blank"
+          className="flex items-center gap-2 text-xs rounded-lg border p-2 bg-slate-900 hover:bg-slate-700 text-white dark:bg-sky-700 dark:hover:bg-sky-600 border-black dark:border-solid transition-colors duration-700"
+        >
           <GrDownload size={20} />
-          Download CV
-        </button>
+          {downloadCV}
+        </Link>
       </div>
     </section>
   );
