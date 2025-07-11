6af7188ee86c342dd5d2a9598d54608275cea986
Cristian Arando <crisarandosyse@gmail.com>
2025-07-11 11:50:47 -0400

    Refactor astro.config.mjs to include path aliasing and update robots.txt with correct sitemap URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import {fileURLToPath} from "url";
+import path from "path";
+
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src")
+      }
+    }
   },
 
   i18n: {
